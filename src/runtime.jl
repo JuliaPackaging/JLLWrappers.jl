0f0bc2058cf2e2ea42173362885aa8f9c2f65494
Elliot Saba <staticfloat@gmail.com>
2020-10-13 14:06:05 -0700

    Add `Sys.BINDIR` to `JULIA_LIBDIRS` on Windows

--- src/runtime.jl
+++ src/runtime.jl
@@ -85,6 +85,10 @@ Return the library paths that e.g. libjulia and such are stored in.
 function get_julia_libpaths()
     if isempty(JULIA_LIBDIRS)
         append!(JULIA_LIBDIRS, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+        # Windows needs to see the BINDIR as well
+        @static if Sys.iswindows()
+            push!(JULIA_LIBDIRS, Sys.BINDIR)
+        end
     end
     return JULIA_LIBDIRS
 end
