20a9c743ba5d93dcfa5d7fa0dff84cfe9d05a167
Elliot Saba <staticfloat@gmail.com>
2023-07-29 12:11:16 -0400

    Update tests
    This updates tests to download test JLLs directly, rather than embedding
    them, making it a lot easier to update to support new platforms.
    For truly old versions of Julia (e.g. `v1.3`) we have "special" tags
    that we download that don't have platforms like `aarch64-apple-darwin`
    embedded within them.

--- test/OpenLibm_jll/src/OpenLibm_jll.jl
+++
@@ -1,9 +0,0 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule OpenLibm_jll
-using Base
-using Base: UUID
-import JLLWrappers
-
-JLLWrappers.@generate_main_file_header("OpenLibm")
-JLLWrappers.@generate_main_file("OpenLibm", UUID("05823500-19ac-5b8b-9628-191a04bc5112"))
-end  # module OpenLibm_jll
